108a269a38a7bb27e60489abdc24b72c64475ac1
darshanbh <darshanbh8@gmail.com>
2026-03-06 23:47:02 +0530

    problems on array

---
+++ youtube_video_learning/colour_pair_matching.java
@@ -0,0 +1,15 @@
+package youtube_video_learning;
+import java.util.*;
+public class colour_pair_matching {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-- > 0){
+            int N=sc.nextInt();
+            int result=N+1;
+            System.out.println(result);
+           
+        }
+    }
+    
+}
